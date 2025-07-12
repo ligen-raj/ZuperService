03c5ab645eb9235b9c4e0e2629b92ce04bb18976
Ligen Raj <rnlrligen@gmail.com>
2025-07-12 10:32:19 +0530

    first commit

---
+++ ZuperServices/ZuperServicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZuperServicesApp.swift
+//  ZuperServices
+//
+//  Created by Ligen Raj on 12/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ZuperServicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
